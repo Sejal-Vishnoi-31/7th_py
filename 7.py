d05148399b0c00f685a409a42c596ee592d325df
sejal <sejalvishnoi32@gmail.com>
2023-04-12 21:40:29 +0530

    This commit is quiz

---
+++ 7.py
@@ -0,0 +1,9 @@
+#SOLVE QUIZ
+print("Enter the age")
+age = int(input())
+if age<18:
+    print("you cannot drive")
+elif age ==18:
+    print("we will think about you")
+else:
+    print("you can drive")        
